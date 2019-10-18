1477745ffe5bc42aa48003cf007a354ee29592a9
pamcuellas <pamcuellas@gmail.com>
2019-10-18 12:43:59 -0400

    Starting step 2.

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,136 +1,105 @@
-// Function to determine marker size based on population
-function markerSize(population) {
-  return population / 40;
-}
+/* jshint esversion: 6*/ 
 
-// An array containing all of the information needed to create city and state markers
-var locations = [
-  {
-    coordinates: [40.7128, -74.0059],
-    state: {
-      name: "New York State",
-      population: 19795791
-    },
-    city: {
-      name: "New York",
-      population: 8550405
-    }
-  },
-  {
-    coordinates: [34.0522, -118.2437],
-    state: {
-      name: "California",
-      population: 39250017
-    },
-    city: {
-      name: "Lost Angeles",
-      population: 3971883
-    }
-  },
-  {
-    coordinates: [41.8781, -87.6298],
-    state: {
-      name: "Michigan",
-      population: 9928300
-    },
-    city: {
-      name: "Chicago",
-      population: 2720546
-    }
-  },
-  {
-    coordinates: [29.7604, -95.3698],
-    state: {
-      name: "Texas",
-      population: 26960000
-    },
-    city: {
-      name: "Houston",
-      population: 2296224
-    }
-  },
-  {
-    coordinates: [41.2524, -95.9980],
-    state: {
-      name: "Nebraska",
-      population: 1882000
-    },
-    city: {
-      name: "Omaha",
-      population: 446599
-    }
-  }
-];
+// URL to get the Earthquake data.
+const earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Define arrays to hold created city and state markers
-var cityMarkers = [];
-var stateMarkers = [];
-
-// Loop through locations and create city and state markers
-for (var i = 0; i < locations.length; i++) {
-  // Setting the marker radius for the state by passing population into the markerSize function
-  stateMarkers.push(
-    L.circle(locations[i].coordinates, {
-      stroke: false,
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: "white",
-      radius: markerSize(locations[i].state.population)
-    })
-  );
-
-  // Setting the marker radius for the city by passing population into the markerSize function
-  cityMarkers.push(
-    L.circle(locations[i].coordinates, {
-      stroke: false,
-      fillOpacity: 0.75,
-      color: "purple",
-      fillColor: "purple",
-      radius: markerSize(locations[i].city.population)
-    })
-  );
-}
-
-// Define variables for our base layers
-var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-});
-
-var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.dark",
-  accessToken: API_KEY
-});
-
-// Create two separate layer groups: one for cities and one for states
-var states = L.layerGroup(stateMarkers);
-var cities = L.layerGroup(cityMarkers);
-
-// Create a baseMaps object
-var baseMaps = {
-  "Street Map": streetmap,
-  "Dark Map": darkmap
-};
-
-// Create an overlay object
-var overlayMaps = {
-  "State Population": states,
-  "City Population": cities
-};
-
-// Define a map object
-var myMap = L.map("map", {
+// Create a map object
+var vMap = L.map("map", {
   center: [37.09, -95.71],
-  zoom: 5,
-  layers: [streetmap, states, cities]
+  zoom: 4,
 });
 
-// Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
-}).addTo(myMap);
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets-basic",
+  accessToken: API_KEY
+}).addTo(vMap);
+
+// Function to get Additional Earthquake information and colorScale (source data.js)
+function getInfo(mag, source) {
+  return source.find( obj => (obj.magInterval[0] <= mag && mag < obj.magInterval[1]) );
+}
+
+function addMarkers ( response ) {
+
+  // Loop through the cities array and create one marker for each record object
+  for (var i = 0; i < response.metadata.count; i++) {
+
+    // REMARK: There are some earthquakes with negative magnitudes.
+    // Assign the magnitude to a variable
+    var mag = response.features[i].properties.mag;
+
+    // Conditionals for colors accordingly to the magnitude.
+    var color = getInfo(mag, colorScale).color;
+
+    // Get the coordinates
+    var coordinates = [ response.features[i].geometry.coordinates[1], response.features[i].geometry.coordinates[0]];
+    
+    // Get more information to present on tooltip.
+    var moreInfo = getInfo(mag, additionalEarthquakeInfo);
+
+    // Add circles to map
+    L.circle(coordinates, {
+      stroke: true,
+      fillOpacity: 0.7,
+      weight: 1,
+      color: "white",
+      fillColor: color,
+      radius: mag * 25000
+    }).bindPopup("<h3>" + response.features[i].properties.place + "</h3>" +
+                 "<hr>" +
+                 "<span>Magnitude: " + mag + "</span><br>" + 
+                 "<span>Effect: " + moreInfo.effect + "</span><br>" +
+                 "<span>Estimate number each year: " + moreInfo.freqYear + "</span>"
+                 ).addTo(vMap);
+  }
+}
+
+
+// Perform the API call to get data
+d3.json(earthquakeURL,  addMarkers );
+
+// Create a legend for color scale
+var info = L.control({
+  position: "bottomright"
+});
+
+// When the layer control is added, insert a div with the class of "color-scale"
+info.onAdd = function() {
+  var div = L.DomUtil.create("div", "color-scale");
+  return div;
+};
+
+// Add the legend to the map
+info.addTo(vMap);
+
+// Legend for color scale.
+document.querySelector(".color-scale").innerHTML = [
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(0,colorScale).color + ";'></div><p class='color-range'>0-1</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(1,colorScale).color + ";'></div><p class='color-range'>1-2</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(2,colorScale).color + ";'></div><p class='color-range'>2-3</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(3,colorScale).color + ";'></div><p class='color-range'>3-4</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(4,colorScale).color + ";'></div><p class='color-range'>4-5</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(5,colorScale).color + ";'></div><p class='color-range'> 5+</p></div>"
+].join("");
+
+
+// Uncomment this link local geojson for when data.beta.nyc is down
+var link = "./static/data/PB2002_plates.json";
+
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a geoJSON layer with the retrieved data
+  L.geoJson(data, {
+    style: function(feature) {
+      return {
+        color: "rgba(0,100,100, 0.5)",
+        fillOpacity: 0,
+        weight: 1
+      };
+    }
+  }).addTo(vMap);
+});
+
