b8d312e9cd790aa5fe86dd1f29a2edee6b7c67b3
pamcuellas <pamcuellas@gmail.com>
2019-10-18 08:39:29 -0400

    Leaflet step 1 finished.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,5 +1,8 @@
 /* jshint esversion: 6*/ 
 
+// URL to get the Earthquake data.
+const earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
 // Create a map object
 var vMap = L.map("map", {
   center: [37.09, -95.71],
@@ -14,20 +17,9 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(vMap);
 
-
-//  Additional Earthquake information. Source: http://www.geo.mtu.edu/UPSeis/magnitude.html
-var additionalEarthquakeInfo = [ 
-{magInterval: [-1000, 2.5 ], effect:"Usually not felt, but can be recorded by seismograph.", freqYear: "900,000"},
-{magInterval: [2.5  , 5.5 ], effect:"Often felt, but only causes minor damage.",             freqYear: "30,000"},
-{magInterval: [5.5  , 6.1 ], effect:"Slight damage to buildings and other structures.",      freqYear: "500"},
-{magInterval: [6.1  , 7.0 ], effect:"May cause a lot of damage in very populated areas.",    freqYear: "100"},
-{magInterval: [7.0  , 8.0 ], effect:"Serious damage.",                                       freqYear: "20"},
-{magInterval: [8.0  , 1000],	effect:"Can totally destroy communities near the epicenter.",  freqYear: "One every 5 to 10 years"}
-];
-
-// Function to get Additional Earthquake information
-function getInfo(mag) {
-  return additionalEarthquakeInfo.find( obj => (obj.magInterval[0] <= mag && mag < obj.magInterval[1]) );
+// Function to get Additional Earthquake information and colorScale (source data.js)
+function getInfo(mag, source) {
+  return source.find( obj => (obj.magInterval[0] <= mag && mag < obj.magInterval[1]) );
 }
 
 function addMarkers ( response ) {
@@ -35,42 +27,23 @@ function addMarkers ( response ) {
   // Loop through the cities array and create one marker for each record object
   for (var i = 0; i < response.metadata.count; i++) {
 
-    // REMARKS: There are some earthquakes with negative magnitudes.
-
+    // REMARK: There are some earthquakes with negative magnitudes.
     // Assign the magnitude to a variable
     var mag = response.features[i].properties.mag;
 
-    // Conditionals for colors
-    var color = "";
-    if (mag <= 1) {
-      color = "#e474d6";
-    }
-    else if (mag <= 2) {
-      color = "#e474d6";
-    }
-    else if (mag <= 3 ) {
-      color = "#e474d6";
-    }
-    else if (mag <= 4 ) {
-      color = "#d732d1";
-    }
-    else if (mag <= 5 ) {
-      color = "#5b005b";
-    }
-    else {
-      color = "#340034";
-    }
+    // Conditionals for colors accordingly to the magnitude.
+    var color = getInfo(mag, colorScale).color;
 
     // Get the coordinates
     var coordinates = [ response.features[i].geometry.coordinates[1], response.features[i].geometry.coordinates[0]];
     
-
-    var moreInfo = getInfo(mag);
+    // Get more information to present on tooltip.
+    var moreInfo = getInfo(mag, additionalEarthquakeInfo);
 
     // Add circles to map
     L.circle(coordinates, {
       stroke: true,
-      fillOpacity: 0.5,
+      fillOpacity: 0.7,
       weight: 1,
       color: "white",
       fillColor: color,
@@ -84,29 +57,30 @@ function addMarkers ( response ) {
   }
 }
 
-const earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-//const earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-// Perform an API call to the Citi Bike Station Information endpoint
+
+// Perform the API call to get data
 d3.json(earthquakeURL,  addMarkers );
 
-// Create a legend to display information about our map
+// Create a legend for color scale
 var info = L.control({
   position: "bottomright"
 });
 
-// When the layer control is added, insert a div with the class of "legend"
+// When the layer control is added, insert a div with the class of "color-scale"
 info.onAdd = function() {
   var div = L.DomUtil.create("div", "color-scale");
   return div;
 };
-// Add the info color-scale to the map
+
+// Add the legend to the map
 info.addTo(vMap);
 
+// Legend for color scale.
 document.querySelector(".color-scale").innerHTML = [
-  "<span class='color1'></span><p class='color-range'>0 - 1</p>",
-  "<span class='color2'></span><p class='color-range'>1 - 2</p>",
-  "<span class='color3'></span><p class='color-range'>2 - 3</p>",
-  "<span class='color4'></span><p class='color-range'>3 - 4</p>",
-  "<span class='color5'></span><p class='color-range'>4 - 5</p>",
-  "<span class='color6'></span><p class='color-range'> 5+  </p>"
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(0,colorScale).color + ";'></div><p class='color-range'>0-1</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(1,colorScale).color + ";'></div><p class='color-range'>1-2</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(2,colorScale).color + ";'></div><p class='color-range'>2-3</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(3,colorScale).color + ";'></div><p class='color-range'>3-4</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(4,colorScale).color + ";'></div><p class='color-range'>4-5</p></div>",
+  "<div class='color-range-container'><div class='box-color' style='background:" + getInfo(5,colorScale).color + ";'></div><p class='color-range'> 5+</p></div>"
 ].join("");
